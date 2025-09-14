1afb6b627100c797ecc7c533aa9bd94c252db943
jont129 <jonmartinez592@gmail.com>
2025-09-13 18:22:39 -0600

    Estructura inicial del proyecto VetBackend

---
+++ src/test/java/com/Vet/VetBackend/VetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vet.VetBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
