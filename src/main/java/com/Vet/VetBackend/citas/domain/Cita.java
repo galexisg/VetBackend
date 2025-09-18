b87826ff7b14a090386b87b016c3150309ac8e8f
JustV-svg <robertillo1805@gmail.com>
2025-09-18 11:47:26 -0600

    Creación de la entidad de Cita

---
+++ src/main/java/com/Vet/VetBackend/citas/domain/Cita.java
@@ -0,0 +1,140 @@
+package com.Vet.VetBackend.citas.domain;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cita")
+public class Cita {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cita_id")
+    private Long citaId;
+
+    @Column(name = "mascota_id", nullable = true)
+    private Integer mascotaId;
+
+    @Column(name = "usuario_id", nullable = true)
+    private Integer usuarioId;
+
+    @Column(name = "veterinario_id", nullable = true)
+    private Short veterinarioId;
+
+    @Column(name = "motivo_id", nullable = true)
+    private Byte motivoId;
+
+    @Column(name = "cita_estado_id", nullable = true)
+    private int citaEstadoId;
+
+
+    @Column(name = "factura_id", nullable = true)
+    private Long facturaId;
+
+    @Column(name = "consulta_id", nullable = true)
+    private Long consultaId;
+
+    public enum Tipo {
+        Normal, Emergencia, Control
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", nullable = false, columnDefinition = "ENUM('Normal','Emergencia','Control') DEFAULT 'Normal'")
+    private Tipo tipo = Tipo.Normal;
+
+    @Column(name = "fecha_hora", nullable = false)
+    private LocalDateTime fechaHora;
+
+    @Column(name = "observaciones", length = 250, nullable = true)
+    private String observaciones;
+
+    // --- Getters y Setters ---
+    public Long getCitaId() {
+        return citaId;
+    }
+
+    public void setCitaId(Long citaId) {
+        this.citaId = citaId;
+    }
+
+    public Integer getMascotaId() {
+        return mascotaId;
+    }
+
+    public void setMascotaId(Integer mascotaId) {
+        this.mascotaId = mascotaId;
+    }
+
+    public Integer getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Integer usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Short getVeterinarioId() {
+        return veterinarioId;
+    }
+
+    public void setVeterinarioId(Short veterinarioId) {
+        this.veterinarioId = veterinarioId;
+    }
+
+    public Byte getMotivoId() {
+        return motivoId;
+    }
+
+    public void setMotivoId(Byte motivoId) {
+        this.motivoId = motivoId;
+    }
+
+    public int getCitaEstadoId() {
+        return citaEstadoId;
+    }
+
+    public void setCitaEstadoId(Byte citaEstadoId) {
+        this.citaEstadoId = citaEstadoId;
+    }
+
+
+    public Long getFacturaId() {
+        return facturaId;
+    }
+
+    public void setFacturaId(Long facturaId) {
+        this.facturaId = facturaId;
+    }
+
+    public Long getConsultaId() {
+        return consultaId;
+    }
+
+    public void setConsultaId(Long consultaId) {
+        this.consultaId = consultaId;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+}
