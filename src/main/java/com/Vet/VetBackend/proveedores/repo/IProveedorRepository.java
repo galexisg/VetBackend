150f67defdeb44d2d0fa1ea340d6bdc0e7fe2d2f
oswaldo844 <hs24001@esfe.agape.edu.sv>
2025-09-17 21:55:57 -0600

    "realice la tabla de proveedores"

---
+++ src/main/java/com/Vet/VetBackend/proveedores/repo/IProveedorRepository.java
@@ -0,0 +1,7 @@
+package com.Vet.VetBackend.proveedores.repo;
+
+import com.Vet.VetBackend.proveedores.domain.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProveedorRepository  extends JpaRepository<Proveedor, Integer>{
+}
---
+++ src/main/java/com/Vet/VetBackend/proveedores/web/dto/ProveedorModificar.java
@@ -0,0 +1,21 @@
+package com.Vet.VetBackend.proveedores.web.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Getter
+@Setter
+
+
+public class ProveedorModificar  implements  Serializable{
+    private  Integer id;
+    private String nombre;
+    private String nit;
+    private String telefono;
+    private String email;
+    private String direccion;
+    private Integer estadoid;  // 👈 Campo agregado, puede ser NULL
+    private String notas;
+
+}
