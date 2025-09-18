ba5e6709e81fd16fd6457aa657cb6555e60a6fc5
David Chavez <adonayco49@hotmail.com>
2025-09-17 22:21:23 -0600

    Creacion de Backend(API) BloqueHorario

--- src/main/java/com/Vet/VetBackend/agenda/domain/BloqueHorario.java
+++ src/main/java/com/Vet/VetBackend/agenda/domain/BloqueHorario.java
@@ -1,4 +1,28 @@
 package com.Vet.VetBackend.agenda.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "BloqueHorario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BloqueHorario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BloqueHorarioId", nullable = false)
+    private Integer bloqueHorarioId;
+
+    @Column(name = "Inicio", nullable = false)
+    private LocalTime inicio;
+
+    @Column(name = "Fin", nullable = false)
+    private LocalTime fin;
+
+    @Column(name = "Activo", nullable = false)
+    private Boolean activo = true; // 🔹 eliminación lógica
 }
--- src/main/java/com/Vet/VetBackend/agenda/web/dto/BloqueHorarioModificarReq.java
+++ src/main/java/com/Vet/VetBackend/agenda/web/dto/BloqueHorarioModificarReq.java
@@ -1,4 +1,14 @@
 package com.Vet.VetBackend.agenda.web.dto;
 
+import lombok.*;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BloqueHorarioModificarReq {
+    private Integer  id;
+    private LocalTime inicio;
+    private LocalTime fin;
 }
