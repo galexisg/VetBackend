91c05cb7cb6c32a00d68a26046dcd6b6f5254ad9
Josue Escobar <JosueEscobar000@hotmail.com>
2025-09-18 20:47:32 -0600

    Actualizando Tabla veterinario

--- src/main/java/com/Vet/VetBackend/veterinario/repo/VeterinarioRepository.java
+++ src/main/java/com/Vet/VetBackend/veterinario/repo/VeterinarioRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface VeterinarioRepository extends JpaRepository<Veterinario, Integer> {
-    List<Veterinario> findByEstadoId(int estadoId);
 }
 
