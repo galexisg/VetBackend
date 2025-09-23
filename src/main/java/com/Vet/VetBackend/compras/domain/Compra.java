866e056b82f6c60ccbe62dd01f81aad36b769e0b
Valencia <kvalenciahernandez44@hotmail.com>
2025-09-23 13:24:20 -0600

    Inventario casi terminado

--- src/main/java/com/Vet/VetBackend/compras/domain/Compra.java
+++ src/main/java/com/Vet/VetBackend/compras/domain/Compra.java
@@ -1,6 +1,5 @@
 package com.Vet.VetBackend.compras.domain;
 
-
 import com.Vet.VetBackend.proveedores.domain.Proveedor;
 import com.Vet.VetBackend.usuarios.domain.Usuario;
 import jakarta.persistence.*;
@@ -23,17 +22,26 @@ public class Compra {
     @Column(name = "fecha")
     private LocalDate fecha;
 
-    @Column(name = "descripcion")
+    @Column(name = "descripcion", length = 255)
     private String descripcion;
 
     @Column(name = "total", nullable = false)
     private double total;
 
     @ManyToOne
-    @JoinColumn(name = "proveedor_id", foreignKey = @ForeignKey(name = "fk_compra__proveedor"))
+    @JoinColumn(
+            name = "proveedor_id",
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "fk_compra__proveedor")
+    )
     private Proveedor proveedor;
 
     @ManyToOne
-    @JoinColumn(name = "usuario_id", foreignKey = @ForeignKey(name = "fk_compra_usuario"))
+    @JoinColumn(
+            name = "usuario_id",
+            referencedColumnName = "UsuarioId",
+            nullable = true,
+            foreignKey = @ForeignKey(name = "fk_compra_usuario")
+    )
     private Usuario usuario;
 }
--- src/main/java/com/Vet/VetBackend/compras/web/dto/CompraObtener.java
+++ src/main/java/com/Vet/VetBackend/compras/web/dto/CompraObtener.java
@@ -1,17 +1,16 @@
 package com.Vet.VetBackend.compras.web.dto;
 
-
-import java.io.Serializable;
-import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
+import java.io.Serializable;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class CompraObtener implements Serializable {
     private Integer id;
-    private Long proveedorId;
-    // private Long usuarioId; // 🔜 Se mostrará cuando esté disponible
+    private Integer proveedorId;
+    private Integer usuarioId;
     private LocalDate fecha;
     private String descripcion;
     private double total;
