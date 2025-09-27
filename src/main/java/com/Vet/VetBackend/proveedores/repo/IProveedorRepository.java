6d02d9152b1789ac7ce0aaae20874d1306ac54bb
JosueEsc7676 <JosueEscobar000@hotmail.com>
2025-09-27 11:32:00 -0600

    Delete src/main/java/com/Vet/VetBackend/proveedores directory

--- src/main/java/com/Vet/VetBackend/proveedores/repo/IProveedorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Vet.VetBackend.proveedores.repo;
-
-import com.Vet.VetBackend.proveedores.domain.Proveedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProveedorRepository extends JpaRepository<Proveedor, Integer> {
-}
