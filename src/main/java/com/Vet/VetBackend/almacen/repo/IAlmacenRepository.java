a53fadf8e11958359d72a6874ffa4aae4ae5b504
JosueEsc7676 <JosueEscobar000@hotmail.com>
2025-09-27 11:30:22 -0600

    Delete src/main/java/com/Vet/VetBackend/almacen directory

--- src/main/java/com/Vet/VetBackend/almacen/repo/IAlmacenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Vet.VetBackend.almacen.repo;
-
-import com.Vet.VetBackend.almacen.domain.Almacen;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAlmacenRepository extends JpaRepository<Almacen, Integer> {
-
-}
